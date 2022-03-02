292877f6001789d3a7be5344172b3d2d70e07ed3
Bradford Gill <bradford42400@gmail.com>
2022-03-02 00:20:58 -0500

    add pistreaming

---
+++ pistreaming/server.py
@@ -0,0 +1,226 @@
+"""
+I have modified this slightly to preform
+Copyright 2014 Dave Hughes <dave@waveform.org.uk>
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holder nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+"""
+
+import sys
+import io
+import os
+import shutil
+from subprocess import Popen, PIPE
+from string import Template
+from struct import Struct
+from threading import Thread
+from time import sleep, time
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from wsgiref.simple_server import make_server
+
+from infer import ObjectDetector
+import cv2
+import numpy as np
+
+import picamera
+from ws4py.websocket import WebSocket
+from ws4py.server.wsgirefserver import (
+    WSGIServer,
+    WebSocketWSGIHandler,
+    WebSocketWSGIRequestHandler,
+)
+from ws4py.server.wsgiutils import WebSocketWSGIApplication
+
+###########################################
+# CONFIGURATION
+WIDTH = 640
+HEIGHT = 480
+FRAMERATE = 2
+HTTP_PORT = 8082
+WS_PORT = 8084
+COLOR = u'#444'
+BGCOLOR = u'#333'
+JSMPEG_MAGIC = b'jsmp'
+JSMPEG_HEADER = Struct('>4sHH')
+VFLIP = False
+HFLIP = False
+
+###########################################
+def get_opencv_img_from_buffer(buffer, flags):
+    bytes_as_np_array = np.frombuffer(buffer.read(), dtype=np.uint8)
+    return cv2.imdecode(bytes_as_np_array, flags)
+
+class StreamingHttpHandler(BaseHTTPRequestHandler):
+    def do_HEAD(self):
+        self.do_GET()
+
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+            return
+        elif self.path == '/jsmpg.js':
+            content_type = 'application/javascript'
+            content = self.server.jsmpg_content
+        elif self.path == '/index.html':
+            content_type = 'text/html; charset=utf-8'
+            tpl = Template(self.server.index_template)
+            content = tpl.safe_substitute(dict(
+                WS_PORT=WS_PORT, WIDTH=WIDTH, HEIGHT=HEIGHT, COLOR=COLOR,
+                BGCOLOR=BGCOLOR))
+        else:
+            self.send_error(404, 'File not found')
+            return
+        content = content.encode('utf-8')
+        self.send_response(200)
+        self.send_header('Content-Type', content_type)
+        self.send_header('Content-Length', len(content))
+        self.send_header('Last-Modified', self.date_time_string(time()))
+        self.end_headers()
+        if self.command == 'GET':
+            self.wfile.write(content)
+
+
+class StreamingHttpServer(HTTPServer):
+    def __init__(self):
+        super(StreamingHttpServer, self).__init__(
+                ('', HTTP_PORT), StreamingHttpHandler)
+        with io.open('index.html', 'r') as f:
+            self.index_template = f.read()
+        with io.open('jsmpg.js', 'r') as f:
+            self.jsmpg_content = f.read()
+
+
+class StreamingWebSocket(WebSocket):
+    def opened(self):
+        self.send(JSMPEG_HEADER.pack(JSMPEG_MAGIC, WIDTH, HEIGHT), binary=True)
+
+
+class BroadcastOutput(object):
+    def __init__(self, camera, object_detector):
+        print('Spawning background conversion process')
+        self.converter = Popen([
+            'ffmpeg',
+            '-f', 'rawvideo',
+            '-pix_fmt', 'jpg',
+            '-s', '%dx%d' % camera.resolution,
+            '-r', str(float(camera.framerate)),
+            '-i', '-',
+            '-f', 'mpeg1video',
+            '-b', '800k',
+            '-r', str(float(camera.framerate)),
+            '-'],
+            stdin=PIPE, stdout=PIPE, stderr=io.open(os.devnull, 'wb'),
+            shell=False, close_fds=True)
+
+        self.object_detector = object_detector
+
+    def write(self, b):
+        if b.startswith(b'\xff\xd8'):
+            img = get_opencv_img_from_buffer(b, cv2.IMREAD_COLOR)
+            out = self.object_detector(img)
+            is_success, buffer = cv2.imencode(".jpg", out)
+            b = io.BytesIO(buffer)
+        self.converter.stdin.write(b)
+
+    def flush(self):
+        print('Waiting for background conversion process to exit')
+        self.converter.stdin.close()
+        self.converter.wait()
+
+
+class BroadcastThread(Thread):
+    def __init__(self, converter, websocket_server):
+        super(BroadcastThread, self).__init__()
+        self.converter = converter
+        self.websocket_server = websocket_server
+
+    def run(self):
+        try:
+            while True:
+                buf = self.converter.stdout.read1(32768)
+                if buf:
+                    self.websocket_server.manager.broadcast(buf, binary=True)
+                elif self.converter.poll() is not None:
+                    break
+        finally:
+            self.converter.stdout.close()
+
+
+def main():
+    print("loading net onto device")
+    object_detector = ObjectDetector('yolov5n')
+    print('Initializing camera')
+    with picamera.PiCamera() as camera:
+        camera.resolution = (WIDTH, HEIGHT)
+        camera.framerate = FRAMERATE
+        camera.vflip = VFLIP # flips image rightside up, as needed
+        camera.hflip = HFLIP # flips image left-right, as needed
+        sleep(1) # camera warm-up time
+        print('Initializing websockets server on port %d' % WS_PORT)
+        WebSocketWSGIHandler.http_version = '1.1'
+        websocket_server = make_server(
+            '', WS_PORT,
+            server_class=WSGIServer,
+            handler_class=WebSocketWSGIRequestHandler,
+            app=WebSocketWSGIApplication(handler_cls=StreamingWebSocket))
+        websocket_server.initialize_websockets_manager()
+        websocket_thread = Thread(target=websocket_server.serve_forever)
+        print('Initializing HTTP server on port %d' % HTTP_PORT)
+        http_server = StreamingHttpServer()
+        http_thread = Thread(target=http_server.serve_forever)
+        print('Initializing broadcast thread')
+        output = BroadcastOutput(camera, object_detector)
+        broadcast_thread = BroadcastThread(output.converter, websocket_server)
+        print('Starting recording')
+        camera.start_recording(output, 'jpg')
+        try:
+            print('Starting websockets thread')
+            websocket_thread.start()
+            print('Starting HTTP server thread')
+            http_thread.start()
+            print('Starting broadcast thread')
+            broadcast_thread.start()
+            while True:
+                camera.wait_recording(1)
+        except KeyboardInterrupt:
+            pass
+        finally:
+            print('Stopping recording')
+            camera.stop_recording()
+            print('Waiting for broadcast thread to finish')
+            broadcast_thread.join()
+            print('Shutting down HTTP server')
+            http_server.shutdown()
+            print('Shutting down websockets server')
+            websocket_server.shutdown()
+            print('Waiting for HTTP server thread to finish')
+            http_thread.join()
+            print('Waiting for websockets thread to finish')
+            websocket_thread.join()
+
+
+if __name__ == '__main__':
+    main()
