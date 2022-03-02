062454937ba3a54adb41ebc5ec2b29e3ffa007e6
Bradford Gill <bradford42400@gmail.com>
2022-03-02 18:45:06 -0500

    fix server in minor ways

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import cv2
 from cv2 import exp
-from infer import prep_cv2_img, parse_predictions, create_detector, draw_boxes, non_max_surpression, YOLOV5N_ANCHORS
+from infer import ObjectDetector
 
 from flask import Flask, render_template, Response
 from threading import Lock
@@ -9,62 +9,17 @@ from openvino.inference_engine import IECore
 
 app = Flask(__name__)
 
-lock = Lock()
-
-print('Setting up network for NCS2')
-
-ie = IECore()
-
-device = 'MYRIAD'
 model_name = 'yolov5n'
+device = 'MYRIAD'
 
-net = ie.read_network(model = f'models/{model_name}.xml', weights = f'models/{model_name}.bin')
-exec_net = ie.load_network(network = net, device_name = device, num_requests=2)
+# hack to not load ncs2 more than one
+print('name:' ,__name__)
+if __name__ != '__main__':
+    print('Setting up network for NCS2')
+    object_detector = ObjectDetector(model_name, device)
+else:
+    object_detector = None
 
-
-class ObjectDetector(): # for some reason this needs to be in here
-    """
-    A nice abstraction of the object detector
-    
-    inputs a cv2 image (bgr)
-    outputs a cv2 image, same size as input (bgr)
-
-    design thoughts:
-    i guess this could be a function returning a function rather than a clas
-    """
-    def __init__(self, 
-            model_name, 
-            device = 'CPU',
-            conf_threshold = .5, 
-            iou_threshold = .5,
-            num_classes = 80,
-            anchors = YOLOV5N_ANCHORS,
-            img_size = 640
-            ):
-
-        self.conf_threshold = conf_threshold
-        self.iou_threshold = iou_threshold
-
-        self.detector = create_detector(num_classes = num_classes, anchors = anchors, img_size = img_size)
-    
-    def __call__(self, img):
-        inputs = prep_cv2_img(img)
-        with lock: # prevent multiple calls from happening at once
-            outputs = exec_net.infer(inputs=inputs)
-
-        outputs = [out for _, out in outputs.items()]
-
-        preds = self.detector(outputs)
-
-        objects = parse_predictions(preds, threshold = self.conf_threshold)
-        objects = non_max_surpression(objects, threshold = self.iou_threshold)
-
-        img_out = draw_boxes(img, objects)
-        return img_out
-
-
-
-object_detector = ObjectDetector(model_name, device)
 camera = cv2.VideoCapture(0)
 
 LOGGER = print
