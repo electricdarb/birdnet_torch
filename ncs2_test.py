411ed7d4562550f1f9d506a36bcc400ea6916b6e
Bradford Gill <bradford42400@gmail.com>
2022-03-02 13:41:30 -0500

    uodated a test

--- ncs2_test.py
+++ ncs2_test.py
@@ -12,3 +12,8 @@ exec_net = ie.load_network(network = net, device_name = device, num_requests=2)
 img = np.zeros((1, 3, 640, 640))
 
 sample_out = exec_net.infer(inputs = {'images': img})
+ie = IECore()
+device = 'MYRIAD'
+
+net = ie.read_network(model = f'{model_path}.xml', weights = f'{model_path}.bin')
+exec_net = ie.load_network(network = net, device_name = device, num_requests=2)
