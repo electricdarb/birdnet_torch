b3cae5c558bc7386f02261b75b3c4922e56eadc9
Bradford Gill <bradford42400@gmail.com>
2022-03-02 13:07:41 -0500

    add objdetect class to server.py

--- server.py
+++ server.py
@@ -1,7 +1,54 @@
 import cv2
-from infer import ObjectDetector
+from infer import prep_cv2_img, parse_predictions, create_detector, draw_boxes, non_max_surpression
 
 from flask import Flask, render_template, Response
+
+from openvino.inference_engine import IECore
+
+class ObjectDetector(): # for some reason this needs to be in here
+    """
+    A nice abstraction of the object detector
+    
+    inputs a cv2 image (bgr)
+    outputs a cv2 image, same size as input (bgr)
+
+    design thoughts:
+    i guess this could be a function returning a function rather than a clas
+    """
+    def __init__(self, 
+            model_name, 
+            device = 'MYRIAD',
+            conf_threshold = .5, 
+            iou_threshold = .5,
+            num_classes = 80,
+            anchors = YOLOV5N_ANCHORS,
+            img_size = 640
+            ):
+
+        self.ie = IECore()
+
+        net = self.ie.read_network(model = f'/home/pi/birdnet_torch/models/{model_name}.xml', weights = f'/home/pi/birdnet_torch/models/{model_name}.bin')
+        self.net = self.ie.load_network(network = net, device_name = device, num_requests=2)
+
+        self.conf_threshold = conf_threshold
+        self.iou_threshold = iou_threshold
+
+        self.detector = create_detector(num_classes = num_classes, anchors = anchors, img_size = img_size)
+    
+    def __call__(self, img):
+        inputs = prep_cv2_img(img)
+
+        outputs = self.net.infer(inputs=inputs)
+        outputs = [out for _, out in outputs.items()]
+
+        preds = self.detector(outputs)
+
+        objects = parse_predictions(preds, threshold = self.conf_threshold)
+        objects = non_max_surpression(objects, threshold = self.iou_threshold)
+
+        img_out = draw_boxes(img, objects)
+        return img_out
+
 
 device = 'MYRIAD'
 model_name = 'yolov5n'
